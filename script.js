a535f7fb7b28dae8c5830740c653cc8aa80ae47c
Samantha Hirsch <sammyhirsch0109@gmail.com>
2024-01-25 13:15:19 -0500

    finished js

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
 document.getElementById('totalBillResult').innerText = 'Bill: $' + bill.toFixed(2)
 document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amtPerPers.toFixed(2)
 }
+
+//helper function
+function calculateTip(bill, quality) {
+    if (quality === 'Great'){
+       return bill * .2
+    } else if (quality === 'Good'){
+        return bill * .15
+    } else if (quality === 'Poor'){
+        return bill * .10
+    }
+}
+
+function calculateTotalBill(bill, tip){
+    return bill + tip
+}
+
+function calcAmtPerPers(totalBill, numOfPeople){
+    return totalBill/numOfPeople 
+}
